5dd7354b2d8505860d629a79e6244cbaa304a5c8
olga <bend_sinister@mail.ru>
2022-09-19 10:58:47 +0300

    T01 comparing two numbers

---
+++ Task01/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+*/
+
+Console.Write("Введите значение числа А: ");
+int userNumberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение числа В: ");
+int userNumberB = Convert.ToInt32(Console.ReadLine());
+
+if(userNumberA == userNumberB)
+
+    Console.Write("Числа равны: А = В");
+    
+else  if(userNumberA > userNumberB)
+        
+         Console.Write($"Большее число А: {userNumberA}. Меньшее число В:  {userNumberB}");
+        
+else Console.Write($"Большее число B: {userNumberB}. Меньшее число A:  {userNumberA}");
+    
