74bee020c32e09f7f2cd68d382b8845127d6aba7
olga <bend_sinister@mail.ru>
2022-09-19 19:15:56 +0300

    T04 четные числа

---
+++ Task04/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача:   Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+*/
+Console.Write("Введите целое значение N: ");
+int userNumberN = Convert.ToInt32(Console.ReadLine());
+
+int count = 2;
+
+if(userNumberN > 1)
+
+
+while (count <= userNumberN)
+{
+    Console.Write($"{count}, ");
+    count = count + 2;
+}
+      
+else Console.Write("К сожалению, с такими входными данными задача не имеет решения");
