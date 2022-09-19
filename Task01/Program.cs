47464a30a14cc06370e2322484915a2cc3938650
olga <bend_sinister@mail.ru>
2022-09-19 11:44:13 +0300

    T01 уточнили категорию чисел

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -2,15 +2,15 @@
 Задача: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 */
 
-Console.Write("Введите значение числа А: ");
+Console.Write("Введите значение целого числа А: ");
 int userNumberA = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите значение числа В: ");
+Console.Write("Введите значение целого числа В: ");
 int userNumberB = Convert.ToInt32(Console.ReadLine());
 
 if(userNumberA == userNumberB)
 
     Console.Write("Числа равны: А = В");
-    
+
 else  if(userNumberA > userNumberB)
         
          Console.Write($"Большее число А: {userNumberA}. Меньшее число В:  {userNumberB}");
